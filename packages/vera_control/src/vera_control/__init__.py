c212379913fe77f4646fba5391765471ba6f7a4c
Patrick Sims <234958733+thebrazenbeard@users.noreply.github.com>
2026-09-23 15:45:55 -0400

    Initialize Vera control package

---
+++ packages/vera_control/src/vera_control/__init__.py
@@ -0,0 +1,5 @@
+"""Local Vera control-plane architecture resources."""
+
+from .resources import load_json_resource, load_text_resource, resource_path
+
+__all__ = ["load_json_resource", "load_text_resource", "resource_path"]
