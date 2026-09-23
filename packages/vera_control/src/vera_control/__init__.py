0d0e9e6513dbf95dd7d3c427cb88f305f878bb6d
Patrick Sims <234958733+thebrazenbeard@users.noreply.github.com>
2026-09-23 15:56:42 -0400

    Expose monorepo-local R10 control closure

--- packages/vera_control/src/vera_control/__init__.py
+++ packages/vera_control/src/vera_control/__init__.py
@@ -1,5 +1,17 @@
 """Local Vera control-plane architecture resources."""
 
+from .local_profile import (
+    R10_SOURCE_CLOSURE,
+    local_r10_source_paths,
+    validate_local_r10_source_closure,
+)
 from .resources import load_json_resource, load_text_resource, resource_path
 
-__all__ = ["load_json_resource", "load_text_resource", "resource_path"]
+__all__ = [
+    "R10_SOURCE_CLOSURE",
+    "load_json_resource",
+    "load_text_resource",
+    "local_r10_source_paths",
+    "resource_path",
+    "validate_local_r10_source_closure",
+]
