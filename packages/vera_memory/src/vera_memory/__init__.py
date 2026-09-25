d547ddbf7b4b514708c9739412088e85c38fda1b
Patrick Sims <234958733+thebrazenbeard@users.noreply.github.com>
2026-09-25 18:33:30 -0400

    feat: expose learned influence gate

--- packages/vera_memory/src/vera_memory/__init__.py
+++ packages/vera_memory/src/vera_memory/__init__.py
@@ -8,10 +8,29 @@ from .ledger import (
     StaleMemoryHead,
 )
 
+from .learned_influence import (
+    LearnedInfluenceBlocked,
+    LearnedInfluenceError,
+    LearnedInfluenceGate,
+    LearnedInfluenceReceipt,
+    LearnedInfluenceReplay,
+    LearnedInfluenceStale,
+    LearnedRevision,
+    ReviewDisposition,
+)
+
 __all__ = [
     "AdmissionRequest",
     "MemoryClass",
     "MemoryLedger",
     "MemoryRecord",
     "StaleMemoryHead",
+    "LearnedInfluenceBlocked",
+    "LearnedInfluenceError",
+    "LearnedInfluenceGate",
+    "LearnedInfluenceReceipt",
+    "LearnedInfluenceReplay",
+    "LearnedInfluenceStale",
+    "LearnedRevision",
+    "ReviewDisposition",
 ]
